d9216d34c63472bf79c43bf8aa4f24a2a632bdb6
yuan <mengyuan@neurongenius.com>
2022-04-15 18:50:54 +0800

    first meng

---
+++ index.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const axios = require('axios');
+
+const port = 8090;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/plain')
+  res.end('time--out--\n')
+})
+
+axios.get('https://sctapi.ftqq.com/SCT139165TckU4uUruV1kYAmZhSIEYyUk9.send?title=hellomengyuan')
+  .then(response => {
+    console.log('success push')
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+
+
+server.listen(port, () => {
+  console.log(`运行port: ${port}`)
+})
